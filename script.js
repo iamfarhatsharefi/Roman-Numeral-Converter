b482a54a403449d650a4502e49d787e27e729ef1
iamfarhatsharefi <sharefifarhat@gmail.com>
2024-06-25 10:49:37 -0700

    adding some changes to css,linter issue

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+function convertToRoman(num) {
+  const romanNumerals = [
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' },
+  ];
+
+  let result = '';
+  for (const { value, numeral } of romanNumerals) {
+    while (num >= value) {
+      result += numeral;
+      num -= value;
+    }
+  }
+    return result;
+}
+
 document.getElementById('convert-btn').addEventListener('click', () => {
   const inputElement = document.getElementById('number');
   const outputElement = document.getElementById('output');
@@ -20,30 +47,3 @@ document.getElementById('convert-btn').addEventListener('click', () => {
   
   outputElement.textContent = convertToRoman(inputValue);
 });
-
-function convertToRoman(num) {
-  const romanNumerals = [
-    { value: 1000, numeral: 'M' },
-    { value: 900, numeral: 'CM' },
-    { value: 500, numeral: 'D' },
-    { value: 400, numeral: 'CD' },
-    { value: 100, numeral: 'C' },
-    { value: 90, numeral: 'XC' },
-    { value: 50, numeral: 'L' },
-    { value: 40, numeral: 'XL' },
-    { value: 10, numeral: 'X' },
-    { value: 9, numeral: 'IX' },
-    { value: 5, numeral: 'V' },
-    { value: 4, numeral: 'IV' },
-    { value: 1, numeral: 'I' },
-  ];
-  
-  let result = '';
-  for (const { value, numeral } of romanNumerals) {
-    while (num >= value) {
-      result += numeral;
-      num -= value;
-    }
-  }
-  return result;
-}
